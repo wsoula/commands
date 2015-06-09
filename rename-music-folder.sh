45524c8c1a77a15ca2d5f6de6ff21e85bc1457ac
Will Soula <william.soula@drillinginfo.com>
2015-06-08 20:39:25 -0500

    Add check for blank artist or album

--- rename-music-folder.sh
+++ rename-music-folder.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -e
 
 if [[ -f tmp ]]; then
-  echo file
   rm tmp
-else
-  echo no file
 fi
 file="`ls | grep -m 1 -o .*mp3 | head -1`"
 echo file is $file
 ffmpeg -i "$file" -f ffmetadata tmp
 if [[ -f tmp2 ]]; then
-  echo file
   rm tmp2
-else
-  echo no file
 fi
 tail -n +2 tmp > tmp2
 sed -ibak s/=/=\"/g tmp2
 sed -ibak s/$/\"/g tmp2
 . tmp2
 rm tmp tmp2 tmp2bak
+if [[ "" == "$artist" ]]; then
+  echo NO VALUE FOR ARTIST.  EXITING
+  exit 1
+fi
+if [[ "" == "$album" ]]; then
+  echo NO VALUE FOR ALBUM TITLE.  EXITING
+  exit 1
+fi
 mkdir "../$artist - $album"
 mv * "../$artist - $album"
 dir=`pwd`
